364dca567857cfd3ab39c6da95359b7d616150ec
Arina <arina.bubenina@mail.ru>
2026-03-06 19:07:13 +0700

    add gemmtr.h

---
+++ gemmtr_BLAS/gemmtr.h
@@ -0,0 +1,25 @@
+#pragma once
+
+void sgemmtr(
+int n,
+int m,
+int k,
+float alpha,
+const float* A,
+const float* B,
+float beta,
+float* C,
+bool upper
+);
+
+void dgemmtr(
+int n,
+int m,
+int k,
+double alpha,
+const double* A,
+const double* B,
+double beta,
+double* C,
+bool upper
+);
