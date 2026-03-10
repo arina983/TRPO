e48199a5ebc3e65a31b41795968decea25b8624d
Arina <arina.bubenina@mail.ru>
2026-03-10 20:03:17 +0700

    test.cpp

--- gemmtr_BLAS/test.cpp
+++ gemmtr_BLAS/test.cpp
@@ -5,15 +5,28 @@ int main() {
 
     int n = 2;
 
-    float A[4] = {1,2,3,4};
-    float B[4] = {5,6,7,8};
-    float C[4] = {0};
+    float A_s[4] = {1,2,3,4};
+    float B_s[4] = {5,6,7,8};
+    float C_s[4] = {0};
 
-    sgemmtr(n, n, n, 1, A, B, 0, C, true);
+    double A_d[4] = {1,2,3,4};
+    double B_d[4] = {5,6,7,8};
+    double C_d[4] = {0};
 
-    for(int i=0;i<4;i++)
-    std::cout << C[i] << " ";
+    sgemmtr(n, n, n, 1, A_s, B_s, 0, C_s, true);
+    dgemmtr(n, n, n, 1, A_d, B_d, 0, C_d, true);
+
+    std::cout << "sgemmtr: ";
+    for(int i = 0; i < 4; i++)
+        std::cout << C_s[i] << " ";
     std::cout << std::endl;
+
+    std::cout << "dgemmtr: ";
+    for(int i = 0; i < 4; i++)
+        std::cout << C_d[i] << " ";
+    std::cout << std::endl;
+
+    return 0;
 
 }
  
